14828de981c8d1481e0d9d1424ac30668ff26564
NizTheDev <arehman08@hotmail.com>
2023-03-19 21:55:48 +0500

    feat: documentation page

---
+++ src/app/components/DocumentationTabs.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { nodejs, python } from "@/helpers/documentation-code";
+import SimpleBar from "simplebar-react";
+import { Code } from "./ui/Code";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/Tabs";
+
+export const DocumentationTabs = () => {
+  return (
+    <Tabs defaultValue="nodejs" className="max-w-2xl w-full">
+      <TabsList>
+        <TabsTrigger value="nodejs">NodeJS</TabsTrigger>
+        <TabsTrigger value="python">Python</TabsTrigger>
+      </TabsList>
+      <TabsContent value="nodejs">
+        <SimpleBar forceVisible="y">
+          <Code animated code={nodejs} language="javascript" show />
+        </SimpleBar>
+      </TabsContent>
+      <TabsContent value="python">
+        <SimpleBar forceVisible="y">
+          <Code animated code={python} language="python" show />
+        </SimpleBar>
+      </TabsContent>
+    </Tabs>
+  );
+};
